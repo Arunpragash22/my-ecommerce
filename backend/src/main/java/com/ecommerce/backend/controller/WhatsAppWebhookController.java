22acbf39cb7ba17fa16e0edf6c5e1e9ea7d4a78f
Dell <vimalanarunpragash2000@gmail.com>
2026-08-10 11:40:22 +0530

    Fix WhatsApp webhook JsonNode

--- backend/src/main/java/com/ecommerce/backend/controller/WhatsAppWebhookController.java
+++ backend/src/main/java/com/ecommerce/backend/controller/WhatsAppWebhookController.java
@@ -1,6 +1,6 @@
 package com.ecommerce.backend.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import tools.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
